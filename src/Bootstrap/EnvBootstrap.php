d1f7b8b1b38b50d3fab41e3076aa65740b2082f3
tg666 <tomasglawaty@icloud.com>
2020-11-17 22:26:06 +0100

    EnvBootstrap improvements
    - an argument `$rootDir` is now optional, the directory is automatically detected by default
    - changed typehint for an argument `$debugModeDetectors` to `iterable`

--- src/Bootstrap/EnvBootstrap.php
+++ src/Bootstrap/EnvBootstrap.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace SixtyEightPublishers\Environment\Bootstrap;
 
+use ReflectionClass;
 use RuntimeException;
 use Nette\Configurator;
+use ReflectionException;
+use Composer\Autoload\ClassLoader;
 use Symfony\Component\Dotenv\Dotenv;
 
 final class EnvBootstrap
@@ -25,12 +28,12 @@ final class EnvBootstrap
 	}
 
 	/**
-	 * @param string                                                               $rootDir
-	 * @param \SixtyEightPublishers\Environment\Debug\DebugModeDetectorInterface[] $debugModeDetectors
+	 * @param iterable|\SixtyEightPublishers\Environment\Debug\DebugModeDetectorInterface[] $debugModeDetectors
+	 * @param string|NULL                                                                   $rootDir
 	 *
 	 * @return array
 	 */
-	public static function boot(string $rootDir, array $debugModeDetectors = []): array
+	public static function boot(iterable $debugModeDetectors = [], ?string $rootDir = NULL): array
 	{
 		self::loadEnv($rootDir);
 		self::detectDebugMode($debugModeDetectors);
@@ -39,15 +42,15 @@ final class EnvBootstrap
 	}
 
 	/**
-	 * @param \Nette\Configurator $configurator
-	 * @param string              $rootDir
-	 * @param array               $debugModeDetectors
+	 * @param \Nette\Configurator                                                           $configurator
+	 * @param iterable|\SixtyEightPublishers\Environment\Debug\DebugModeDetectorInterface[] $debugModeDetectors
+	 * @param string|NULL                                                                   $rootDir
 	 *
 	 * @return array
 	 */
-	public static function bootNetteConfigurator(Configurator $configurator, string $rootDir, array $debugModeDetectors = []): array
+	public static function bootNetteConfigurator(Configurator $configurator, iterable $debugModeDetectors = [], ?string $rootDir = NULL): array
 	{
-		$env = self::boot($rootDir, $debugModeDetectors);
+		$env = self::boot($debugModeDetectors, $rootDir);
 
 		$configurator->setDebugMode((bool) $env[self::APP_DEBUG]);
 		$configurator->addDynamicParameters([
@@ -58,13 +61,15 @@ final class EnvBootstrap
 	}
 
 	/**
-	 * @param string $rootDir
+	 * @param string|NULL $rootDir
 	 *
 	 * @return void
 	 * @throws \RuntimeException
 	 */
-	public static function loadEnv(string $rootDir): void
+	public static function loadEnv(?string $rootDir = NULL): void
 	{
+		$rootDir = self::resolveRootDir($rootDir);
+
 		if (is_array($env = @include $rootDir . '/.env.local.php') && ($_SERVER[self::APP_ENV] ?? $_ENV[self::APP_ENV] ?? $env[self::APP_ENV]) === $env[self::APP_ENV]) {
 			foreach ($env as $k => $v) {
 				$_ENV[$k] = $_ENV[$k] ?? (isset($_SERVER[$k]) && 0 !== strpos($k, 'HTTP_') ? $_SERVER[$k] : $v);
@@ -84,11 +89,11 @@ final class EnvBootstrap
 	}
 
 	/**
-	 * @param array $debugModeDetectors
+	 * @param iterable|\SixtyEightPublishers\Environment\Debug\DebugModeDetectorInterface[] $debugModeDetectors
 	 *
 	 * @return bool
 	 */
-	public static function detectDebugMode(array $debugModeDetectors = []): bool
+	public static function detectDebugMode(iterable $debugModeDetectors = []): bool
 	{
 		$debug = FALSE;
 
@@ -103,5 +108,33 @@ final class EnvBootstrap
 		$_SERVER[self::APP_DEBUG] = $_ENV[self::APP_DEBUG] = $debug ? '1' : '0';
 
 		return $debug;
+	}
+
+	/**
+	 * @param string|NULL $rootDir
+	 *
+	 * @return string
+	 * @throws \RuntimeException
+	 */
+	private static function resolveRootDir(?string $rootDir = NULL): string
+	{
+		if (NULL !== $rootDir) {
+			return $rootDir;
+		}
+
+		if (!class_exists(ClassLoader::class)) {
+			throw new RuntimeException(sprintf(
+				'Project root directory can\'t be detected because the class %s can\'t be found. Please provide the root directory manually.',
+				ClassLoader::class
+			));
+		}
+
+		try {
+			$reflection = new ReflectionClass(ClassLoader::class);
+
+			return dirname($reflection->getFileName(), 3);
+		} catch (ReflectionException $e) {
+			throw new RuntimeException('Project root directory can\'t be detected. Please provide the root directory manually.', 0, $e);
+		}
 	}
 }
